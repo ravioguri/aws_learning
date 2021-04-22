bbcc442b9d127876f81db0f7dbdca0815d224308
ravioguri <82867020+ravioguri@users.noreply.github.com>
2021-04-22 16:19:13 +0530

    edit 1

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+#import boto3
+
+app = Flask(__name__)
+
+@app.route("/home")
+def homepage():
+    return "AWS Assignment experiments"
+
+
+@app.route("/home/aws")
+def app_s3():
+    import boto3
+    client = boto3.client('s3', 
+    aws_access_key_id=AKIA57YKW6WAG5UXYEUF, 
+    aws_secret_access_key = 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)
+    response = client.list_buckets()
+
+    buckets=[]
+    for bucket in response['Buckets']:
+        buckets.append(bucket["Name"])
+
+    for i in buckets:
+        print(i)
+
+        echo = client.list_objects(Bucket=i)
+
+        bucketfiles = []
+
+        for i in range(len(echo['Contents'])):
+            bucketfiles.append(echo.get('Contents', [])[i]['Key'])
+            print(bucketfiles[i])
+
+        
+    print(bucketfiles)
+
+
+'''@app.route("/home/<folder_name>")
+def home():
+    s3_obj = boto3.resource('s3')'''
+    
